66e6f4b4ea0598b2107bd8b5a84b507a54542b1a
xpaula760-beep <xpaula760@gmail.com>
2026-03-17 15:20:42 -0700

    ok

--- src/app/services/package.api.js
+++ src/app/services/package.api.js
@@ -1,12 +1,4 @@
-import axios from "axios";
-
-const rawBase = process.env.NEXT_PUBLIC_PACKAGES_API_URL || "";
-const normalizedBase = rawBase ? rawBase.replace(/\/$/, "") + "/api" : "/api";
-
-const api = axios.create({
-  baseURL: normalizedBase,
-  withCredentials: true
-});
+import api from "@/lib/axios";
 
 /* ===========================
    PACKAGE API
